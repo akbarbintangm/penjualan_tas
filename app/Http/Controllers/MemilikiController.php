3f9504d3dca10e8426dd66b478761857bad4f8b3
akbarbintangm <akbarbintangmahendra@gmail.com>
2021-12-24 21:06:25 +0700

    update

---
+++ app/Http/Controllers/MemilikiController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Memiliki;
+use Illuminate\Http\Request;
+
+class MemilikiController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Memiliki  $memiliki
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Memiliki $memiliki)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Memiliki  $memiliki
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Memiliki $memiliki)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Memiliki  $memiliki
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Memiliki $memiliki)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Memiliki  $memiliki
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Memiliki $memiliki)
+    {
+        //
+    }
+}
